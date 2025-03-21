42bf1a305031b91cfcc3f50b768d27e94e9ee5c4
Louis Terrats <louis.terrats@gmail.com>
2025-03-21 16:19:44 +0100

    Update

--- test.py
+++ test.py
@@ -5,14 +5,11 @@ Created on Wed Jan  8 12:41:48 2025
 
 @author: terrats
 """
-
-# =============================================================================
-# ### Import libraries and set paths
-# =============================================================================
+#%% Import libraries
 
 # working_directory = "/data/home/terrats/" # '/data/home/terrats/' # on rs-pro machine  
-path_to_RIOMAR_package = "/home/terrats/Desktop/RIOMAR/PACKAGE/myRIOMAR_dev"
-# path_to_RIOMAR_package = "/media/shared_storage/Documents/CODING/PROJECTS/myRIOMAR_dev"
+# path_to_RIOMAR_package = "/home/terrats/Desktop/RIOMAR/PACKAGE/myRIOMAR_dev"
+path_to_RIOMAR_package = "/media/shared_storage/Documents/CODING/PROJECTS/myRIOMAR_dev"
 
 import sys
 sys.path.append(path_to_RIOMAR_package)
@@ -25,40 +22,48 @@ import matplotlib
 # mpl.use('module://matplotlib_inline.backend_inline') # To show plots on the Plot panel (be careful as it consumes RAM memory !)
 matplotlib.use('agg') # Prevent showing plot in the Plot panel (this saves RAM memory)
 
-# =============================================================================
-# ### Apply the functions
-# =============================================================================    
 
-core_arguments = {  'Data_sources': ['EUMETSAT'],
-                    'Sensor_names':["olcia"],
-                    'Satellite_variables':['CHLA'],
-                    'Atmospheric_corrections':["polymer"],
-                    'Temporal_resolution': ["DAILY"],
+#%% Set core parameters
+core_arguments = {  'Data_sources': ['SEXTANT'], # ODATIS / SEXTANT / EUMETSAT
+                    'Sensor_names':["merged", "modis"], 
+                    'Satellite_variables':['CHLA', 'SPM'], 
+                    'Atmospheric_corrections':["Standard"], # Standard (SEXTANT / EUMETSAT) /  ...
+                    'Temporal_resolution': ["DAILY"], # A enlever ! 
                     'start_day' : '2018/07/31',
                     'end_day' : '2018/08/01'}
 
+#%% Downlaod data
 
 myRIOMAR._0_data_downloading.Download_satellite_data(core_arguments,
-                                                     nb_of_cores_to_use = 2,
+                                                     nb_of_cores_to_use = 1,
                                                      overwrite_existing_satellite_files = False,
                                                      where_to_save_satellite_data = "/media/shared_storage/Documents/CODING/TEST/SATELLITE_DATA")
 
+#%% Downlaod data
 myRIOMAR._0_data_downloading.Plot_and_Save_the_map(core_arguments,
                                                    nb_of_cores_to_use = 6,
                                                    where_are_saved_satellite_data = "/media/shared_storage/Documents/CODING/TEST/SATELLITE_DATA",
                                                    start_day_of_maps_to_plot = '2018/07/31',
                                                    end_day_of_maps_to_plot = '2018/08/01')
 
+#%%
+
+myRIOMAR._Add_my_zone(name = 'THAY', min_lat = 9, ...)
+
 myRIOMAR._1_data_validation.Match_up_with_insitu_measurements(core_arguments, 
                                                               redo_the_MU_database = False, 
                                                               nb_of_cores_to_use = 6,
-                                                              where_to_save_Match_Up_data = "/home/terrats/Desktop/RIOMAR/TEST/MATCH_UP_DATA/")
+                                                              where_to_save_Match_Up_data = "/home/terrats/Desktop/RIOMAR/TEST/MATCH_UP_DATA/",
+                                                              Zones = "THAU")
 
+#%%
 
 myRIOMAR._2_regional_maps.create_regional_maps(core_arguments,
                                                Zones = ['BAY_OF_BISCAY', 'BAY_OF_SEINE', 'GULF_OF_LION'],
                                                overwrite_existing_regional_maps = True,
                                                plot_the_daily_regional_maps = False)
+
+#%%
 
 # myRIOMAR._2_regional_maps.QC_of_regional_maps(arguments.update({'nb_of_cores_to_use' : 4,
 #                                                                  "Zones" : ['BAY_OF_BISCAY']}) or arguments)
