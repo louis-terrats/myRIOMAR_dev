41f2b2da020f25cfa6014e62bb78120639a1ce88
Louis Terrats <louis.terrats@gmail.com>
2025-04-28 18:18:43 +0200

    Update

---
+++ myRIOMAR/__init__.py
@@ -0,0 +1,4 @@
+# from myRIOMAR_dev import CODES._0_data_downloading, CODES._1_data_validation, CODES._2_regional_maps, CODES._3_plume_detection, CODES._4_X11_analysis, DATA, CODES._5_Figures_for_article
+from myRIOMAR_dev import CODES, DATA, TASKS
+
+# from myRIOMAR_dev import TASKS.task
